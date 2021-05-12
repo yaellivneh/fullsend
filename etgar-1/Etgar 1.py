3f53862724a4f53ba30aef0e16b2417550217ca0
yaellivneh <82993382+yaellivneh@users.noreply.github.com>
2021-05-11 19:15:16 -0700

    Update Etgar 1.py

--- etgar-1/Etgar 1.py
+++ etgar-1/Etgar 1.py
@@ -1,3 +1,13 @@
 import cgi
 form = cgi.FieldStorage()
 searchterm = form.getvalue('answerone')
+
+While True:
+x = input('answerone')
+if x != "egg"
+    print("That is incorrect")
+    continue
+else
+    break
+
+print("That is correct")
